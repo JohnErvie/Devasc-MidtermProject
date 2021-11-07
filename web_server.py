5338c2064f8fc0a220f33de9beeaff8448d0d843
Ervie John Villareal <qejtvillareal@tip.edu.ph>
2021-11-07 15:31:20 +0800

    added connection of html

--- web_server.py
+++ web_server.py
@@ -1,5 +1,5 @@
 # Add to this file for the sample app lab
-from flask import Flask
+from flask import Flask, url_for, redirect
 from flask import request
 from flask import render_template
 
@@ -9,6 +9,18 @@ webApp = Flask(__name__)
 @webApp.route("/")
 def main():
     return render_template("index.html")
+
+@webApp.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('main'))
+
+    # show the form, it wasn't submitted
+    return render_template('test.html')
     
 if __name__ == "__main__":
     webApp.run(host="127.0.0.1", port=8080)
